3c291b2ad477c4645a245ddf2834f5d2c9feab55
Ching Chou <chingchou34@hotmail.com>
2021-06-23 11:14:36 +0200

    asdf
    asdf

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -65,17 +65,14 @@ let columnConfig = {};
 
 
 
-
 // POST requests only work to the parent app, so we call and pass the app from the parent module
 let initApp = (parentApp) => {
     app = parentApp;
 
     // Set the column configuration
     app.post("/upload/columns", (req, res) => {
-
         let reqFile = req.query.file;
         columnConfig[reqFile] = req.body;
-    
         return res.status(200).end();
     });
 };
@@ -83,7 +80,6 @@ let initApp = (parentApp) => {
 
 // Download request for the dataset
 router.get("/download/dataset", (req, res) => {
-
     let reqFile = req.query.file;
     let filePath = "./src/TestFiles/" + reqFile;
 
@@ -102,7 +98,6 @@ router.get("/download/dataset", (req, res) => {
         return res.end();
     }
 
-
     // Parse the dataset
     let config = columnConfig[reqFile];
 
@@ -110,9 +105,44 @@ router.get("/download/dataset", (req, res) => {
     let dataLines = fileData.split("\r\n");
     let columnList = dataLines.shift().split(",");
     if(dataLines[dataLines.length-1] === "") dataLines.pop();
-
+    
     let nodes = {};
+    let edgeInfo = {};
     let edges = {};
+    let attrInfo = {max: {}, min: {}}
+
+    // Parse the node attributes
+    let nodeAttr = Object.keys(config.nodeAttr);
+
+    let fromAttr = nodeAttr.map((attr) => config.nodeAttr[attr][0]);
+    let toAttr = nodeAttr.map((attr) => config.nodeAttr[attr][1]);
+
+    // Parse the edge attributes
+    let edgeAttr = config.edgeAttr.map((attr) => columnList[attr]);
+
+    // returns true if str represents numeric value
+    function isNumeric(str) {  
+        return  !isNaN(str) && // use type coercion to parse the _entirety_ of the string 
+                !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+    }
+
+    // split edgeAttr to two arrays with ordinal and categorical attributes
+    let edgeAttrCategorical = [];
+    let edgeAttrOrdinal = [];
+    let exampleRow = dataLines[0].split(",");
+    for(var Attr of config.edgeAttr) {
+      if(isNumeric(exampleRow[Attr])) {
+        edgeAttrOrdinal.push(Attr);
+      } else {
+        edgeAttrCategorical.push(Attr);
+      }
+    }
+
+    // create placeholder values for max and min for ordinal edge attributes
+    for(var attrIndex of edgeAttrOrdinal) {
+        attrInfo.max[columnList[attrIndex]] = 0;
+        attrInfo.min[columnList[attrIndex]] = 0;
+    }
 
     // Loop over every entry
     for(var entryLine of dataLines) {
@@ -122,62 +152,125 @@ router.get("/download/dataset", (req, res) => {
         let fromId = entry[config.fromId];
         let toId = entry[config.toId];
 
-        // Parse the node attributes and construct an object
-        let fromAttr = config.fromAttr;
-        let from = { id: fromId, ...addAttr(fromAttr, columnList, entry) };
-        
-        let toAttr = config.toAttr;
-        let to = { id: toId, ...addAttr(toAttr, columnList, entry) };
+        // date is defined explicitely
+        let date = Date.parse(entry[config.date]);
+
+        // construct an node object
+        let from = addAttr(fromAttr, nodeAttr, entry);
+        let to = addAttr(toAttr, nodeAttr, entry);
+
+        // TODO: it's now hardcoded but implement something to detect if there are names.
+        from["Name"] = getNameFromEmail(entry[fromAttr[0]]);
+        to["Name"] = getNameFromEmail(entry[toAttr[0]]);
 
         // If the from/to node does not exist, add it
-        if(!nodes[from.id]) nodes[from.id] = from;
-        if(!nodes[to.id]) nodes[to.id] = to;
+        if(!nodes[fromId]) nodes[fromId] = from;
+        if(!nodes[toId]) nodes[toId] = to;
 
-        // Look if the edge has appeared before
+        // Look if the edgeId has appeared before
         // If not, create an empty placeholder for the edge
         let edgeId = `${fromId}-${toId}`;
-        if(!edges[edgeId]) {
-            edges[edgeId] = { count: 0 };
-            for(var idx in config.edgeAttr) {
-                let attrIndex = config.edgeAttr[idx];
+        if(!edgeInfo[edgeId]) {
+            edgeInfo[edgeId] = { count: 0, edges: {}};
+            for(var idx in edgeAttrOrdinal) {
+                let attrIndex = edgeAttrOrdinal[idx];
                 let attr = columnList[attrIndex];
-                edges[edgeId][attr] = [];
+                edgeInfo[edgeId][attr] = 0;
             }
         }
 
-        // Add the attributes to the edge object
-        edges[edgeId].count++;
-        for(var idx in config.edgeAttr) {
-            let attrIndex = config.edgeAttr[idx];
+        // Add the values of attributes to the existing value so after the loop it becomes the total sum of values
+        edgeInfo[edgeId].count++;
+        for(var idx in edgeAttrOrdinal) {
+            let attrIndex = edgeAttrOrdinal[idx];
             let attr = columnList[attrIndex];
-            let value = entry[attrIndex];
-            edges[edgeId][attr].push(value);
+            let value = parseFloat(entry[attrIndex]);
+            edgeInfo[edgeId][attr] = edgeInfo[edgeId][attr] + value;
+            // Update min/max in attrInfo
+            if(value > attrInfo.max[attr]) attrInfo.max[attr] = value;
+            else if(value < attrInfo.min[attr]) attrInfo.min[attr] = value;
         }
 
+        // Add edge placeholder to edgeInfo if it doesn't exist yet
+        if(!edgeInfo[edgeId]["edges"][date]) {
+          edgeInfo[edgeId]["edges"][date] = [];
+        }
+        
+        // construct an edge object
+        let edge = addAttr(config.edgeAttr, edgeAttr, entry);
+
+        // Add edge with attributes
+        edgeInfo[edgeId]["edges"][date].push(edge);
+
+        // Add date placeholder to edges if it doesn't exist yet
+        if(!edges[date]) {
+          edges[date] = { count : 0, edges : {} }
+        }
+
+        // Add edge placeholder to edges if it doesn't exist yet
+        if(!edges[date]["edges"][edgeId]) {
+          edges[date]["edges"][edgeId] = [];
+        }
+
+        // Add edge to edges and increase count in edges
+        edges[date]["edges"][edgeId].push(edge);
+        edges[date].count++
+
+    }
+    // Average all edgeId attributes
+    for(var edgeId of Object.keys(edgeInfo)) {
+      for(var attrIndex of edgeAttrOrdinal) {
+        let attr = columnList[attrIndex];
+        edgeInfo[edgeId][attr] = edgeInfo[edgeId][attr]/edgeInfo[edgeId].count;
+      }
     }
 
+    // Create sortedArray of dates.
+    let datesSorted = Object.keys(edges).sort((a,b) => b-a);
+
     // Create an incremental ordering for the nodes
-    let orderings = { incremental: [null] };
-    for(var i = 1; i < Object.keys(nodes).length; i++) {
+    let orderings = { incremental: [] };
+    for(var i = 1; i <= Object.keys(nodes).length; i++) {
         orderings.incremental.push(i);
     }
 
+    // Create attribute info object
+    edgeAttrOrdinalNames = edgeAttrOrdinal.map((attr) => columnList[attr]);
+    edgeAttrCategoricalNames = edgeAttrCategorical.map((attr) => columnList[attr]);
+    attrInfo = { ...attrInfo, nodeAttr : nodeAttr, edgeAttrOrdinal : edgeAttrOrdinalNames, edgeAttrCategorical : edgeAttrCategoricalNames, nodeNameDisplay : "Name", nodeColorAttr: "Jobtitle", nodeColorAttrMapping: {'CEO':0,'President':1,'Vice President':2,'Director':3,'Managing Director':4,'Manager':5,'Trader':6,'Employee':6,'In House Lawyer':6,'Unknown':6}};
+
+
+    //console.log("orderings", orderings);
     // Return a 200 OK code and send the dataset to the client
-    res.status(200).json({ nodes, edges, orderings }).end();
+    res.status(200).json({ nodes, edgeInfo, edges, datesSorted, orderings, attrInfo}).end();
 });
 
 // Add attributes to a node
-function addAttr(attrList, columnList, entry) {
+function addAttr(attrList, attrNames , entry) {
     let list = {};
     for(var attrIndex in attrList) {
         let idx = attrList[attrIndex];
 
-        let attr = columnList[idx];
+        let attr = attrNames[attrIndex];
         let value = entry[idx];
         list[attr] = value;
     }
     return list;
 }
+
+
+function titleCase(str) {
+  return str[0].toUpperCase() + str.substring(1);
+}
+
+// converting emails to names
+function getNameFromEmail(email) {
+  let nameParts = email.split("@")[0].split(".");
+  let firstName = titleCase(nameParts[0]);
+  let lastName = titleCase(nameParts[nameParts.length-1]) || "";
+  return firstName + " " + lastName;
+}
+
 
 
 
@@ -264,16 +357,7 @@ router.get('/test/download/csv.json', async function(req, res, next) {
 
 });
 
-function titleCase(str) {
-    return str[0].toUpperCase() + str.substring(1);
-}
 
-function getNameFromEmail(email) {
-    let nameParts = email.split("@")[0].split(".");
-    let firstName = titleCase(nameParts[0]);
-    let lastName = titleCase(nameParts[nameParts.length-1]) || "";
-    return { firstName, lastName };
-}
 
 const ranks = {
     "Unknown": 0,
